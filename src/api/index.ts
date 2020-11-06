f93d5769b88b82ffca06dc369b1481a1c1a88547
saa27 <saakshi27050@gmail.com>
2020-11-06 13:39:33 +0530

    :sparkles: Added routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,30 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 export default (): Router => {
   const app = Router();
 
-  //TODO: add routes here...
+  app.get('/:template', templateHandler);
+  app.post('/:template', customTemplateHandler);
 
   return app;
+};
+
+const templateHandler = (req: Request, res: Response) => {
+  console.log(req.query.theme);
+  console.log(req.query.title);
+  console.log(req.query.subtitle);
+  console.log(req.query.fontSize);
+  console.log(req.query.fileType);
+  console.log(req.params.template);
+  res.sendStatus(200);
+};
+
+const customTemplateHandler = (req: Request, res: Response) => {
+  console.log(req.query.theme);
+  console.log(req.query.title);
+  console.log(req.query.subtitle);
+  console.log(req.query.fontSize);
+  console.log(req.query.fileType);
+  console.log(req.params.template);
+  res.sendStatus(200);
 };
