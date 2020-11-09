40ae9027500102cf7b62e2210f7d14e6e6a4bcc8
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2020-11-09 11:16:48 +0530

    :sparkles: Add final configuration generator
    Add final configuration generator having all required properties

--- src/utilities/sharedUtilities.ts
+++ src/utilities/sharedUtilities.ts
@@ -1,19 +1,39 @@
+import path from 'path'
+
 type RequestConfiguration = {
   theme: string;
   title: string;
   subtitle: string;
   custom: boolean;
+  template: string;
 };
 
-const getRequiredConfiguration = (data: any): RequestConfiguration => {
+type IncomingConfiguration = {
+  theme?: string;
+  title?: string;
+  subtitle?: string;
+  custom?: boolean;
+  template?:string;
+  filePath?:string;
+}
+
+export const getRequiredConfiguration = (data: IncomingConfiguration): RequestConfiguration => {
   const finalConfig = {
-    theme: data.theme ? data.theme : 'white',
+    theme: data.theme ? data.theme : 'light',
     title: data.title ? data.title : 'With love from SRMKZILLA',
     subtitle: data.subtitle ? data.subtitle : '@srmkzilla',
     custom: data.custom ? data.custom : false,
+    template: getTemplateFile(data.custom,data.filePath,data.theme)
   };
 
   return finalConfig;
 };
 
-export default getRequiredConfiguration;
+const getTemplateFile = (custom?:boolean, filePath?:string, theme?:string): string=>{
+  if(custom){
+    return filePath;
+  }
+  else{
+    return path.join(__dirname,"..","assets",theme==='dark'?"default-dark.png":"default-light.png");
+  }
+}
