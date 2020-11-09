4d05094d19ac9c2ffb6472a63f253149ffdb1aa6
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2020-11-09 18:51:36 +0530

    ➖ Remove redundant dependency

--- src/utilities/sharedUtilities.ts
+++ src/utilities/sharedUtilities.ts
@@ -1,4 +1,4 @@
-import path from 'path'
+import { join } from 'path';
 
 type RequestConfiguration = {
   theme: string;
@@ -13,8 +13,8 @@ type IncomingConfiguration = {
   title?: string;
   subtitle?: string;
   custom?: boolean;
-  filePath?:string;
-}
+  filePath?: string;
+};
 
 export const getRequiredConfiguration = (data: IncomingConfiguration): RequestConfiguration => {
   const finalConfig = {
@@ -22,17 +22,16 @@ export const getRequiredConfiguration = (data: IncomingConfiguration): RequestCo
     title: data.title ? data.title : 'With love from SRMKZILLA',
     subtitle: data.subtitle ? data.subtitle : '@srmkzilla',
     custom: data.custom ? data.custom : false,
-    template: getTemplateFile(data.custom,data.filePath,data.theme)
+    template: getTemplateFile(data.custom, data.filePath, data.theme),
   };
 
   return finalConfig;
 };
 
-const getTemplateFile = (custom?:boolean, filePath?:string, theme?:string): string=>{
-  if(custom){
+const getTemplateFile = (custom?: boolean, filePath?: string, theme?: string): string => {
+  if (custom) {
     return filePath;
+  } else {
+    return join(__dirname, '..', 'assets', theme === 'dark' ? 'default-dark.png' : 'default-light.png');
   }
-  else{
-    return path.join(__dirname,"..","assets",theme==='dark'?"default-dark.png":"default-light.png");
-  }
-}
+};
