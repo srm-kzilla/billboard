91e24df8291b1780a90a4c62cfaa856215db2d72
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2020-11-20 00:04:53 +0530

    :bug: Fix redundant database issue
    The database URI is not needed

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,12 +1,10 @@
-import config from '../config';
-import database from './database';
 import express from './express';
 import Logger from './logger';
 import Express from 'express';
 
 export default async ({ expressApp }: { expressApp: Express.Application }): Promise<void> => {
-  await database();
-  Logger.info(`✌️ Connection to database successful: ${config.databaseURL}`);
+  //await database();
+  //Logger.info(`✌️ Connection to database successful: ${config.databaseURL}`);
 
   await express({ app: expressApp });
   Logger.info('✌️ Express loaded');
